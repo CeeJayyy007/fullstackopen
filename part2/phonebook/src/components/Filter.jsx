effa7888ac6859b6c0310d56af779ac5f63f2cd9
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-10-23 03:48:20 -0400

    create filter component

---
+++ part2/phonebook/src/components/Filter.jsx
@@ -0,0 +1,7 @@
+const Filter = ({ newFilter, handleFilterChange }) => (
+  <div>
+    Filter shown with: <input value={newFilter} onChange={handleFilterChange} />
+  </div>
+);
+
+export default Filter;
