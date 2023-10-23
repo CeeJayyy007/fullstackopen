1f2e929741dedf344cf2013bfedc43153f5fafa8
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-10-23 00:20:18 -0400

    complete 2.5: course info separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,40 +1,6 @@
+import Course from "./components/Course";
+
 const Header = ({ name }) => <h1>{name}</h1>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-const Total = ({ parts }) => {
-  console.log(parts);
-  return (
-    <h3>
-      Total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
-    </h3>
-  );
-};
-
-const CourseTitle = ({ name }) => <h3>{name}</h3>;
-
-const Course = ({ course }) => {
-  const { name, parts } = course;
-  return (
-    <>
-      <CourseTitle name={name} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </>
-  );
-};
 
 const App = () => {
   const courses = [
