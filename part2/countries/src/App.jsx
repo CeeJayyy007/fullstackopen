e7e18d3da1ea6c251df6c90fec22f18c6fade2a2
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-10-25 02:10:27 -0400

    create initial countries search funtionality

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import Search from "./components/Search";
+import countriesService from "./services/countries";
+import Display from "./components/Display";
+
+const App = () => {
+  const [countries, setCountries] = useState(null);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    countriesService.getAll().then((initialCountries) => {
+      setCountries(initialCountries);
+    });
+  }, []);
+
+  // do not render anything if countries is still null
+  if (!countries) {
+    return null;
+  }
+
+  const handleChange = (event) => {
+    const query = event.target.value;
+
+    //retrieve filtered countries into an array
+    const filterResult = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(query)
+    );
+
+    setFilteredCountries(filterResult);
+  };
+
+  return (
+    <>
+      <Search text="Find Countries" handleChange={handleChange} />
+      <Display filteredCountries={filteredCountries} />
+    </>
+  );
+};
+
+export default App;
