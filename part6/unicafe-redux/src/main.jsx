58ab177ba29e03c8764c521c5a9619e87317cdfd
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-14 16:37:50 -0500

    subscribe to store

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
+import store from "./store/store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
