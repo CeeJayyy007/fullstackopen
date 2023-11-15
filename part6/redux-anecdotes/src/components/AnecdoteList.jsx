c7ddef59d7916cdb02471693e3e25914c1a3f231
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-15 02:16:43 -0500

    chore: add updated redux state  to anecdote list

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,9 +2,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
 
 const Anecdotelist = () => {
-  const anecdotes = useSelector((state) =>
-    state.sort((a, b) => b.votes - a.votes)
-  );
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
+
+    return filter === ""
+      ? sortedAnecdotes
+      : sortedAnecdotes.filter((anecdote) =>
+          anecdote.content.toLowerCase().includes(filter.toLowerCase())
+        );
+  });
 
   const dispatch = useDispatch();
 
