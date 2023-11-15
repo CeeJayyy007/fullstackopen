f64aea3cf5caf7762d8b6075b7e509f86b05b65b
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-15 14:18:27 -0500

    complete 6.12: better anecdotes, step 10

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,13 @@
 import Anecdotelist from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdotesForm";
 import Filter from "./components/Filter";
+import Notification from "./components/Notification";
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Notification />
       <Filter />
       <Anecdotelist />
       <AnecdoteForm />
