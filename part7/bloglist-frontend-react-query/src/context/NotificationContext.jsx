4251d047e1e5f7376e09ef3b34cf8e1e35b3d435
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-11-25 19:46:56 -0500

    feat: add notification context and provider to app

---
+++ part7/bloglist-frontend-react-query/src/context/NotificationContext.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable indent */
+import { createContext, useReducer, useContext } from "react";
+
+const initialState = { message: "", error: false };
+
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case "SHOW":
+      return action.payload;
+    case "HIDE":
+      return "";
+    default:
+      return state;
+  }
+};
+
+const NotificationContext = createContext();
+
+export const NotificationContextProvider = (props) => {
+  const [notification, notificationDispatch] = useReducer(
+    notificationReducer,
+    initialState
+  );
+
+  return (
+    <NotificationContext.Provider value={[notification, notificationDispatch]}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+};
+
+export const useNotificationValue = () => {
+  const notificationAndDispatch = useContext(NotificationContext);
+  return notificationAndDispatch[0];
+};
+
+export const useNotificationDispatch = () => {
+  const notificationAndDispatch = useContext(NotificationContext);
+  return notificationAndDispatch[1];
+};
+
+export default NotificationContext;
