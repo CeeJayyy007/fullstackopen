eb51b6f84f1d7e0073b6319b8c4c36ee4b0ba520
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-01 01:26:08 -0500

    chore: eslint-disable react/no-unescaped-entities

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import { Routes, Route, Link, useMatch, useNavigate } from "react-router-dom";
